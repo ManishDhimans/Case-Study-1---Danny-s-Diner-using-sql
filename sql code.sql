c7cde2fe37045d7404e43b54d5e3c948349afbf1
Manish kumar Dhiman <153525957+ManishDhimans@users.noreply.github.com>
2023-12-12 23:11:26 +0530

    Danny’s Diner SQL CODE .sql

--- sql code.sql
+++ sql code.sql
@@ -1,16 +1,19 @@
- 1 What is the total amount each customer spent at the restaurant?
+Q 1 What is the total amount each customer spent at the restaurant?
+
 select s.customer_id,sum(m.price) as total_amount 
 from sales s
 join menu m on s.product_id=m.product_id
 group by s.customer_id;
 
-2 answer
+Q 2 How many days has each customer visited the restaurant?
+
 select customer_id, count(distinct(order_date)) 
 as visiting_days 
 from sales
 group by customer_id
 order by visiting_days desc;
-3 answer
+
+Q 3 What was the first item from the menu purchased by each customer?
 select distinct(s.customer_id), m.product_name 
 from sales s
 join menu m on s.product_id=m.product_id
@@ -18,7 +21,8 @@ where s.order_date = any
 (select min(order_date) from sales
 group by customer_id)
 
-4 answer 
+Q 4 What is the most purchased item on the menu and how many times was it purchased by all customers? 
+
 select count(m.product_name) as most_purchased, 
 product_name 
 from sales s
@@ -26,7 +30,8 @@ join menu m on s.product_id = m.product_id
 group by m.product_name
 order by most_purchased desc;
 
-5 answer
+5 Which item was the most popular for each customer?
+
 select count(s.product_id) as count, m.product_name,
 s.customer_id
 from sales s
@@ -34,8 +39,9 @@ join menu m on s.product_id = m.product_id
 group by customer_id,product_name
 order by count desc;
 
-5 answer 2nd way
-with r as
+Q 5 -- 2nd way
+
+with rk as
 (SELECT s.customer_id,m.product_name,
         COUNT(s.product_id) as count,
         dense_RANK() OVER (PARTITION BY s.customer_id ORDER BY COUNT(s.product_id) DESC) AS runk
@@ -45,9 +51,10 @@ ON s.product_id = m.product_id
 GROUP BY s.customer_id, s.product_id, m.product_name
 )
 select customer_id, product_name, count 
-from r where runk =1;
+from rk where runk =1;
 
-6answer 
+Q 6 Which item was purchased first by the customer after they became a member? 
+
 with cte as
 (select s.customer_id,
        m.product_name, s.order_date,
@@ -61,7 +68,8 @@ select customer_id, product_name, order_date,ranks
 from cte 
 where ranks =1;
 
-7answer 
+Q 7 Which item was purchased just before the customer became a member? 
+
 with cte as
 (select s.customer_id,
        m.product_name, s.order_date,mb.join_date,
@@ -75,7 +83,8 @@ select customer_id, product_name,order_date,ranks,join_date
 from cte 
 where ranks =1;
 
-answer8
+Q 8 What is the total items and amount spent for each member before they became a member?
+
 select s.customer_id, count(s.product_id) as total_items, 
 sum(m.price) as total_sales
 from sales s 
@@ -84,7 +93,8 @@ join members mb on s.customer_id=mb.customer_id
 where s.order_date < mb.join_date
 group by s.customer_id;
 
-answer 9
+Q 9 If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
 with cte as
 ( select m.product_id, 
 case  when m.product_id = 1 then m.price*20

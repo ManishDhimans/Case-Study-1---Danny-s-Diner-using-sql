22b1ae5dc7748f909fe8bfebfa30f52801dc6c13
Manish kumar Dhiman <153525957+ManishDhimans@users.noreply.github.com>
2023-12-13 20:39:07 +0530

    Add files via upload

--- Danny’s Diner SQL CODE .sql
+++ Danny’s Diner SQL CODE .sql
@@ -130,3 +130,30 @@ Join Menu M
 On M.product_id = S.product_id
 Where S.order_date < d.last_date
 Group by S.customer_id;
+
+Bonus Questions
+
+Q The following questions are related creating basic data tables that Danny and his team can use 
+ to quickly derive insights without needing to join the underlying tables using SQL.
+
+ Recreate the following table output using the available data: 
+
+ Danny also requires further information about the ranking of customer products, but he purposely 
+ does not need the ranking for non-member purchases so he expects null ranking values for the 
+ records when customers are not yet part of the loyalty program.
+
+with cte as
+(select s.customer_id, s.order_date, m.product_name, m.price,
+   case 
+        when mb.join_date <= s.order_date then 'Y'
+        when mb.join_date > s.order_date then 'N'
+		else 'N'end as member
+from sales s
+join menu m on s.product_id=m.product_id
+left join members mb on s.customer_id=mb.customer_id
+order by s.customer_id)
+select *,
+    case
+        when member = 'N' then null
+        else rank() over(partition by customer_id, member order by order_date) end as ranks
+ from cte;
